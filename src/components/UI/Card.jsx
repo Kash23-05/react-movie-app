6b27d2a361a0ec385bdac8055e0b112dd8496a60
kash2314 <kashishjha85@gmail.com>
2026-01-19 14:04:58 +0530

    fix: react-router navigation on vercel

--- src/components/UI/Card.jsx
+++ src/components/UI/Card.jsx
@@ -1,27 +1,23 @@
 import "./Card.css";
 import { useNavigate } from "react-router-dom";
 
-export const Card = ({movieData}) => {
-  const nevigate = useNavigate()
-    const {Poster , imdbID} = movieData;
+export const Card = ({ movieData }) => {
+  const nevigate = useNavigate();
+  const { Poster, imdbID } = movieData;
   return (
     <li className="hero-container">
       <div className="main-container">
         <div className="poster-container">
-          <img
-            src={Poster}
-            className="poster"
-            alt={imdbID}
-          />
+          <img src={Poster} className="poster" alt={imdbID} />
         </div>
 
         <div className="ticket-container">
           <div className="ticket_content">
-            <a href={`/movie/${imdbID}`}>
-              <button className="ticket_buy-btn">
-                Watch Now
-              </button>
-            </a>
+            <button
+              className="ticket_buy-btn"
+              onClick={() => navigate(`/movie/${imdbID}`)}>
+              Watch Now
+            </button>
           </div>
         </div>
       </div>
